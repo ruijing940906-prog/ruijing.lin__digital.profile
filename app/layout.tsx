c72266be310a1dd80ea35ca8f98bc988a02c1dda
ruijing940906-prog <ruijing940906@gmail.com>
2026-04-04 21:59:57 +0800

    feat: icons and buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
-
+import { RiInstagramFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
+import { MdOutlineWeb } from "react-icons/md";
+import { FaYoutube } from "react-icons/fa";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,49 +34,73 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-      <div className="w-screen h-screen flex flex-col sm:flex-row   bg-gray-200 p-6 gap-6 overflow-hidden">
+      <div className="w-screen h-screen flex flex-col sm:flex-row   bg-[#EAD4AD] p-6 gap-6 overflow-hidden">
 
           {/* 左邊  卡片*/}
           {/* <div className="bg-white w-[300px] h-full  p-4  rounded-2xl flex flex-col items-center overflow-y-auto "> */}
-          <div className="min-w-[320px] w-full sm:w-[320px] h-full rounded-2xl bg-white flex justify-start items-center flex-col p-[30px] pt-[30px] overflow-y-auto overflow-x-hidden">
+          <div className="min-w-[320px] w-full sm:w-[320px] h-full rounded-2xl bg-[#FFFEEE] flex justify-start items-center flex-col p-[30px] pt-[30px] overflow-y-auto overflow-x-hidden">
           <Link href="/home" className=" w-full mt-2 flex justify-center">
-              <div> <Image src="/head.png" alt="image" width={230} height={230} className="rounded-full " /></div>
+              <div> <Image src="/head.png" alt="image" width={230} height={230} className="rounded-full transition-all duration-300 ease-in-out hover:scale-105 active:scale-95 cursor-pointer " /></div>
           </Link>
             
           
           {/* <div className="bg-gray-200 w-[120px] h-[120px] rounded-full ">大頭照</div> */}
           <div className="text-center mt-3 font-bold text-2xl">⋆｡‧˚ 林芮竫 ˚‧｡⋆</div>
           <div className="text-center ">ruijing.lin</div>
-          <div className="text-center mt-6">簡介</div>
+          <div className="text-center text-m mt-2 text-[#B1843D] tracking-widest font-bold">🥨🍪🥖</div>
+          <div className="text-center text-s mt-2 text-[#B1843D]  font-bold">第三個字念ㄐㄧㄥˋ !</div>
 
           {/* 社群連結 */}
-          <div className="flex gap-2 mt-6">
-            <div>社群連結</div>
-            <div>社群連結</div>
-            <div>社群連結</div>
+          <div className="flex gap-3 mt-6">
+
+          <a href="https://www.instagram.com/ruijing.lin__/" target="_blank" className="group">
+          <div className=" w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#FFFEEE] transition-all duration-300 group-hover:bg-[#FFE89E] ease-in-out hover:scale-105 active:scale-95 cursor-pointer ">
+           <div className="background-size:cover;background-position:center"><RiInstagramFill size={24} className=" style= color: #FFE89E   transition-colors group-hover:text-white " /></div>
+           </div>
+          </a>
+           
+          <a href="https://www.facebook.com/ruijing.lin?locale=zh_TW" target="_blank" className="group">
+          <div className=" w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#FFFEEE] transition-all duration-300  group-hover:bg-[#FFE89E] ease-in-out hover:scale-105 active:scale-95 cursor-pointer ">
+           <div className="background-size:cover;background-position:center"><FaFacebook  size={24} className=" style= color: #FFE89E   transition-colors group-hover:text-white " /></div>
+           </div>
+          </a>
+          
+          <a href="https://sites.google.com/view/113102026/home" target="_blank" className="group">
+          <div className=" w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#FFFEEE] transition-all duration-300  group-hover:bg-[#FFE89E] ease-in-out hover:scale-105 active:scale-95 cursor-pointer">
+           <div className="background-size:cover;background-position:center"><MdOutlineWeb  size={24} className=" style= color: #FFE89E   transition-colors group-hover:text-white " /></div>
+           </div>
+          </a>
+
+          <a href="https://youtube.com/channel/UCUEHrzX445cV6bgtKltcLwQ?si=P0kUu9sXheCHsgS8" target="_blank" className="group">
+          <div className=" w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#FFFEEE] transition-all duration-300  group-hover:bg-[#FFE89E] ease-in-out hover:scale-105 active:scale-95 cursor-pointer ">
+           <div className="background-size:cover;background-position:center"><FaYoutube   size={24} className=" style= color: #FFE89E   transition-colors group-hover:text-white " /></div>
+           </div>
+          </a>
+
           </div>
 
-          
+
+          {/* 下方按鈕 */}
           <div className="w-full px-4 mt-2 ">
             <Link href="/about" className="block w-full mt-6">
-              <div className="bg-gray-300 p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold">ABOUT</div>
+              <div className="bg-[#F9F2DC] p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold transition-all duration-300 ease-in-out hover:scale-105 active:scale-95 cursor-pointer">ABOUT</div>
             </Link>
 
-            <Link href="/lifestyle" className="block w-full mt-6">
-              <div className="bg-gray-300 p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold">LIFESTYLE</div>
+            <Link href="/lifelog" className="block w-full mt-6">
+              <div className="bg-[#F9F2DC] p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold transition-all duration-300 ease-in-out hover:scale-105 active:scale-95 cursor-pointer">LIFELOG</div>
             </Link>
             
             <Link href="/visual" className="block w-full mt-6">
-            <div className="bg-gray-300 p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold">VISUAL DESIGN</div>
+            <div className="bg-[#F9F2DC] p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold transition-all duration-300 ease-in-out hover:scale-105 active:scale-95 cursor-pointer">VISUAL DESIGN</div>
             </Link>
             
             
             <Link href="/ui" className="block w-full mt-6">
-            <div className="bg-gray-300 p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold">UI DESIGN</div>
+            <div className="bg-[#F9F2DC] p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold transition-all duration-300 ease-in-out hover:scale-105 active:scale-95 cursor-pointer">UI DESIGN</div>
             </Link>
 
             <Link href="/3D&motion" className="block w-full mt-6">
-            <div className="bg-gray-300 p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold">3D MOTION</div>
+            <div className="bg-[#F9F2DC] p-[16px] py-8 mt-6 rounded-2xl text-center w-full h-full font-bold transition-all duration-300 ease-in-out hover:scale-105 active:scale-95 cursor-pointer">3D MOTION</div>
             </Link>
 
             <div className="h-8 w-full flex-shrink:0"></div>
@@ -84,7 +111,7 @@ export default function RootLayout({
 
 
         {/* 右邊 */}
-        <div className="flex-1 h-full bg-white rounded-[40px] p-12 overflow-y-auto"> 
+        <div className="flex-1 h-full bg-[#FFFEEE] rounded-[40px] p-12 overflow-y-auto"> 
            {children}
            </div>
       
